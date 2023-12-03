1d1341fe0015c730f5a6c0713e631f345aa483dd
Alexandre Leblanc <a.leblanc+github@kitai.dev>
2023-12-02 21:06:49 -0500

    Add day2 solution.

---
+++ src/day2.rs
@@ -0,0 +1,168 @@
+use regex::Regex;
+
+struct GameSet {
+    pub red: u32,
+    pub blue: u32,
+    pub green: u32,
+}
+
+struct Game {
+    pub sets: Vec<GameSet>,
+    pub id: u32,
+}
+
+impl Game {
+    pub fn satisfies_constraints(&self, blue: u32, red: u32, green: u32) -> bool {
+        !self
+            .sets
+            .iter()
+            .any(|set| set.blue > blue || set.red > red || set.green > green)
+    }
+}
+
+pub fn get_valid_games_id_sums(input: &str, blue: u32, red: u32, green: u32) -> u32 {
+    get_games(input)
+        .iter()
+        .filter(|game| game.satisfies_constraints(blue, red, green))
+        .map(|game| game.id)
+        .sum()
+}
+
+pub fn get_games_power(input: &str) -> u32 {
+    get_games(input)
+        .iter()
+        .map(|game| {
+            let blue = game.sets.iter().max_by_key(|set| set.blue).unwrap().blue;
+            let green = game.sets.iter().max_by_key(|set| set.green).unwrap().green;
+            let red = game.sets.iter().max_by_key(|set| set.red).unwrap().red;
+
+            blue * green * red
+        })
+        .sum()
+}
+
+fn get_games(input: &str) -> Vec<Game> {
+    let games_str: Vec<&str> = input.lines().collect();
+
+    games_str
+        .iter()
+        .map(|game| {
+            let bound = game.find(':').unwrap();
+
+            Game {
+                id: game[5..bound].parse::<u32>().unwrap(),
+                sets: get_game_sets_str(game)
+                    .iter()
+                    .map(|set| get_set_values(set))
+                    .collect::<Vec<GameSet>>(),
+            }
+        })
+        .collect::<Vec<Game>>()
+}
+
+fn get_game_sets_str(game: &str) -> Vec<&str> {
+    let sets_start = game.find(':').unwrap() + 1; // Exclude the char itself.
+    let sets = game[sets_start..].split(';').collect();
+
+    sets
+}
+
+fn get_set_values(set: &str) -> GameSet {
+    let pattern = r"(?P<redGroup>\d+ red)|(?P<greenGroup>\d+ green)|(?P<blueGroup>\d+ blue)";
+    let re = Regex::new(pattern).unwrap();
+    let mut result = GameSet {
+        red: 0,
+        blue: 0,
+        green: 0,
+    };
+
+    let group_names: Vec<&str> = re.capture_names().skip(1).map(|x| x.unwrap()).collect();
+
+    for caps in re.captures_iter(set) {
+        for name in &group_names {
+            if let Some(m) = caps.name(name) {
+                let digit_ends = m.as_str().find(' ').unwrap();
+                let value = m.as_str()[..digit_ends].parse::<u32>().unwrap();
+
+                match name {
+                    &"greenGroup" => result.green = value,
+                    &"blueGroup" => result.blue = value,
+                    &"redGroup" => result.red = value,
+                    _ => panic!("Unknown color group {name}"),
+                }
+            }
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_game_set() {
+        let line = "3 blue, 4 red";
+        let game_set = get_set_values(line);
+        assert_eq!(game_set.blue, 3);
+        assert_eq!(game_set.red, 4);
+        assert_eq!(game_set.green, 0);
+    }
+
+    #[test]
+    fn test_get_game_sets() {
+        let sets = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+
+        let values = get_game_sets_str(sets);
+
+        assert_eq!(values[0], " 3 blue, 4 red");
+        assert_eq!(values[1], " 1 red, 2 green, 6 blue");
+        assert_eq!(values[2], " 2 green");
+    }
+
+    #[test]
+    fn test_parse_games_log() {
+        const INPUT: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        let values = get_games(&INPUT);
+
+        assert_eq!(values.len(), 5);
+        assert_eq!(values[0].id, 1);
+        assert_eq!(values[4].id, 5);
+        assert_eq!(values[0].sets[0].blue, 3);
+        assert_eq!(values[0].sets[0].red, 4);
+        assert_eq!(values[0].sets[1].red, 1);
+        assert_eq!(values[0].sets[1].green, 2);
+        assert_eq!(values[0].sets[1].blue, 6);
+        assert_eq!(values[4].sets[0].red, 6);
+        assert_eq!(values[4].sets[0].blue, 1);
+        assert_eq!(values[4].sets[0].green, 3);
+    }
+
+    #[test]
+    fn test_valid_games_id_sum() {
+        const INPUT: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        let sum = get_valid_games_id_sums(&INPUT, 14, 12, 13);
+
+        assert_eq!(sum, 8);
+    }
+
+    #[test]
+    fn test_games_power_sum() {
+        const INPUT: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        let sum = get_games_power(&INPUT);
+
+        assert_eq!(sum, 2286);
+    }
+
+    #[test]
+    fn test_game_satisfies_constraints() {
+        const INPUT: &str = "Game 1: 3 blue, 4 red";
+        let values = get_games(&INPUT);
+
+        assert!(!values[0].satisfies_constraints(4, 1, 0)); // Fails because red.
+        assert!(values[0].satisfies_constraints(4, 5, 1)); // Passes.
+        assert!(!values[0].satisfies_constraints(2, 5, 0)); // Fails because blue.
+        assert!(values[0].satisfies_constraints(3, 4, 0)); // Pass.
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::Read;
 
 mod day1;
+mod day2;
 
 fn solve_day1() {
     let mut file = File::open("day1.txt").unwrap();
@@ -13,6 +14,20 @@ fn solve_day1() {
     println!("Day1: {value}");
 }
 
+fn solve_day2() {
+    let mut file = File::open("day2.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let value_valid_sum =
+        day2::get_valid_games_id_sums(contents.to_ascii_lowercase().as_str(), 14, 12, 13);
+    let value_power = day2::get_games_power(contents.to_ascii_lowercase().as_str());
+
+    println!("Day2 valid sum: {value_valid_sum}");
+    println!("Day2 power: {value_power}");
+}
+
 fn main() {
     solve_day1();
+    solve_day2();
 }
